84143600d889d60639e13c0f6beb8af56f17363e
xiaomoL444 <2802273114@qq.com>
2025-06-12 13:45:55 +0800

    组消息

---
+++ MoBot.Core/Models/Message/GroupMsg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MoBot.Core.Models.Message
+{
+	internal class GroupMsg
+	{
+		[JsonPropertyName("group_id")]
+		public long GroupID { get; }
+
+		public List<MessageSegment> MessageSegment = new List<MessageSegment>();
+	}
+}
